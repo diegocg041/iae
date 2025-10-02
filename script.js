345fa5dc467c1b58e4501ae588dd0961f5ac76f4
diegocg041 <142520543+diegocg041@users.noreply.github.com>
2025-10-02 19:14:19 -0300

    ok

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-const caixaPrincipal= document.querySelector("caixa-pricipal");
-const caixaPerguntas= document. querySelector("caixa-perguntas");
-const caixaAlternativas= document. querySelector("caixa-alternativa");
-const caixaResultado= document.querySelector("caixa-resultado");
-const textoRsultado= document.querySelector('texto-resultado');
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
-
-const Perguntas=[
+const perguntas = [
     {
-     enunciado:"Assim que saiu do colegio você se depara com uma tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele também, gera imagens e audios realistas. Qual é seu pensamento?",
-     alternativas:[
-        {
-            texto:"Isso é assustador",
-            afitrmacao:"No inicio, eu ficaria com medo do que essa tecnologia pode fazer."
-        },
-        {
-            texto:"Isso é incrivel",
-            afitrmacao:"Sempre quis saber como usar uma IA em meu dia a dia."
-        }
-     ]
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
+            }
+        ]
     },
-
     {
         enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
@@ -73,3 +71,42 @@ const Perguntas=[
         ]
     },
 ];
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
